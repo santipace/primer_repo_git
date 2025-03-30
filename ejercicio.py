69fa52455365fdac107cafc8ea123777ff782516
Santiago Pace <pacesantiago@gmail.com>
2025-03-30 12:46:39 -0300

    se modifico todo loko

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,4 @@
-nombre = input("Ingrese su nombre")
-apellido = input("Ingrese su apellido")
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
 materia = "Programacion 1"
 print(f"Hola soy {nombre} {apellido} alumno de {materia}")
